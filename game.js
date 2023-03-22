9b4113067a710d67ad7b6e301ec776b3561e3574
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-03-21 21:47:10 -0400

    message

--- game.js
+++ game.js
@@ -2,22 +2,54 @@
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
+let level = 0;
+let started = false;
+
+$(document).keypress(function() {
+    if (!started) {
+
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
 
 $(".btn").click(function() {
-    let userChosenColour = $(this).attr("id");
 
-    userClickedPattern.push(userChosenColour);
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
+
+    level++;
 
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);;
 
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+
+}
+
+function playSound(name){
+
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
+
+}
+
+function animatePress(currentColour){
+
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+
 }
