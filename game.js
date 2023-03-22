bd6419896004c7c81271ac8ea63853f05020a909
Parisa Ettehad <92958591+parisa-ettd@users.noreply.github.com>
2023-03-21 21:01:35 -0400

    message

--- game.js
+++ game.js
@@ -1,13 +1,23 @@
+
 let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
+let userClickedPattern = [];
 
-function nextSequence(){
-    let randomNumber = Math.floor(Math.random() * 4); 
+$(".btn").click(function() {
+    let userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+});
+
+function nextSequence() {
+
+    let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
 
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
     let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
     audio.play();
 }
